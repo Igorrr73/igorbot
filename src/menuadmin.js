eecdb3c786e7ddb4573876dbbd1048de940c0e53
Igorrr73 <79047456+Igorrr73@users.noreply.github.com>
2021-02-14 11:41:27 -0300

    update menuadmin.js

--- src/menuadmin.js
+++ src/menuadmin.js
@@ -1,5 +1,5 @@
 const menuadmin = (prefix, pushname) => {
- return `oiin aqui e o menu dos admins
+ return `oi, aqui é o menu
  _obs para o bot executar esses comandos e nescessario dar adm para o bot_
  
  ◪ *COMANDO DOS ADMINS*
